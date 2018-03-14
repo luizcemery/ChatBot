6d5616bdb618e0f28b50e9dc42f53a5f1c2f6d19
Lucasbatistasilveira <luk.tsbatista@gmail.com>
2018-03-14 19:41:59 -0300

    Resolvendo merge

--- index.php
+++ index.php
@@ -8,20 +8,19 @@
 </head>
 
 <body>
-
     <div class="chatbox">
         <div id = "chatlogs" class="chatlogs">
+
 
         <div class="chat bot">
             <div class="user-photo"><img src="img/user.png"></div>
             <p class="chat-message">Seja Bem Vindo ao atendimento virtual. Em que posso ajudar?</p>
         </div>
-
         </div>
-            <div class="chat-form">
-                <textarea id="texto" name="texto"></textarea>
-                <button id="submit">Send</button>
-            </div>
+        <div class="chat-form">
+            <textarea id="texto" name="texto"></textarea>
+            <button id="submit">Send</button>
+        </div>
     </div>
 
 <!-- Script que controla as mensagens do chat -->
